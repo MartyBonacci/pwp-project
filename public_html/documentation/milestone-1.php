70a22a720275094edbb168324d3504c97d0964c7
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-01-19 18:15:32 -0700

    recreating project to fix .idea error

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Personal Website Project</title>
+	</head>
+	<body bgcolor="#500202">
+		<p align="middle"
+		 		style="color: ghostwhite;font-size:35px"><b>Jeffrey Brink's Personal Website Project</b>
+		<br>
+		<br>
+		<p style="color:ghostwhite;font-size:35px"><b>Purpose</b> </p>
+		<p style="color: antiquewhite;font-size:20px">This website will serve as a portfolio<br>
+																	for my professional career advancement.</p><br>
+		<p style="color:ghostwhite;font-size:35px"><b>Audience</b>
+		</p>
+		<p style="color:antiquewhite;font-size:20px">This website is designed to<br>
+							appeal to professionals in the<br>
+							web development
+							and web design industries.
+		</p>
+		<br>
+		<p style="color:ghostwhite;font-size:35px;"><b>Target Audiences</b></p>
+			<ol>
+				<li style="color:wheat;font-size:20px">IT hiring managers</li>
+				<li style="color:wheat;font-size:20px">Web development professionals</li>
+				<li style="color:wheat;font-size:20px">web design professionals</li>
+				<li style="color:wheat;font-size:20px">peer networks</li>
+				<li style="color:wheat;font-size:20px">recruiters</li>
+				<li style="color:wheat;font-size:20px">graphic arts professionals</li>
+			</ol>
+		<br>
+		<p style="color:ghostwhite;font-size:35px;"><b>goal</b>
+		</p>
+		<p style="color:wheat;font-size:20px">The main goal of my website will be to supply users<br>
+							the ability to view my work and contact me<br>
+							regarding my work and future employment opportunitits.</p>
+		<p style="color:ghostwhite;font-size:35px;"><b>Persona</b><br>
+		<p style="color:wheat;font-size:25px"> Ralph is 27 years old.  He is the managing partner of a startup looking<br>
+															to hire people to fill key positions within his organization.<br>
+															He is debating whether he should hire a separate front-end designer and developer,<br>
+															or try and find one person that can handle both jobs.<br>
+															He has seen people in the past get hired as both designer and developer,<br>
+															yet struggle to do both jobs well at the same time.
+			<br>
+			<br>Ralph’s organization uses windows 10 machines.<br>
+			Ralph was an excellent designer and coder when he was younger,<br>
+			but now codes very little in his management role.<br>
+			Ralph’s company uses many different programs to design a website,<br>
+			but mainly rely on Adobe products (Photoshop, Dreamweaver etc.)
+			<br>
+			<br>
+			When Ralph visits my site, he will find it easy to navagate and well designed.<br>
+			It will be effortless for him to see my work and decide I am the right fit for the job.
+		</p>
+		<img src="office.jpg" alt="Ralph">
+	</body>
+</html>
